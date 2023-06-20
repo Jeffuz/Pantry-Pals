00ace51e137472e6cdf970cde8b0a83c4a4c0cdc
Jeffuz <jeffzhang0049@gmail.com>
2023-06-20 15:02:20 -0700

    CORS

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Pantry_Pals"  # MongoDB connection URI
 mongo = PyMongo(app)
 

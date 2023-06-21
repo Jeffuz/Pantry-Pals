40164ea68889c5a2726a00673873e6dab9f74cb1
Jeffuz <jeffzhang0049@gmail.com>
2023-06-21 14:24:28 -0700

    comments

--- flask-server/client/src/components/RecipeResults.jsx
+++ flask-server/client/src/components/RecipeResults.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export function RecipeResults() {
-  const location = useLocation();
-  const { searchResults } = location.state || {};
+    // Location object
+    const location = useLocation();
+    // default to empty object
+    const { searchResults } = location.state || {};
 
-  return (
-    <div>
-      {searchResults && searchResults.length > 0 ? (
+    return (
         <div>
-          {searchResults.map((recipe, index) => (
-            <div key={index}>
-              <p>{recipe.title}</p>
-            </div>
-          ))}
+            {searchResults && searchResults.length > 0 ? (
+                <div>
+                    {searchResults.map((recipe, index) => (
+                        <div key={index}>
+                            <p>{recipe.title}</p>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No recipes found</p>
+            )}
         </div>
-      ) : (
-        <p>No recipes found</p>
-      )}
-    </div>
-  );
+    );
 }

94e7b9aceeb709c4854459939629f404caeb48ca
Jeffuz <jeffzhang0049@gmail.com>
2023-06-23 19:00:29 -0700

    adding filter component

--- flask-server/client/src/pages/Filter.js
+++ flask-server/client/src/pages/Filter.js
@@ -1,10 +1,11 @@
-
+import { FilterComponent } from "../components/RecipeFilter";
 
 
 export default function Filter() {
     return (
         <div>
             <h1>Filtering Page</h1>
+            <FilterComponent/>
         </div>
     );
 }

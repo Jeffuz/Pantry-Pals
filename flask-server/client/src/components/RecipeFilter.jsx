66313cb6559cdcc801897e9ca17f7bf6c928f311
Jeffuz <jeffzhang0049@gmail.com>
2023-06-23 18:15:11 -0700

    filtering based on enter key pressed

--- flask-server/client/src/components/RecipeFilter.jsx
+++ flask-server/client/src/components/RecipeFilter.jsx
@@ -1,52 +1,60 @@
 import React, { useState } from 'react';
 
 export function FilterComponent() {
-  const [ingredients, setIngredients] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
-  function handleInputChange(event) {
-    const { value } = event.target;
-    setInputValue(value);
-
-    if (value.endsWith(' ')) {
-      // Remove the trailing space and add the ingredient to the list
-      const ingredient = value.trim();
-      if (ingredient !== '') {
-        setIngredients(prevIngredients => [...prevIngredients, ingredient]);
-      }
-
-      // Reset the input value
-      setInputValue('');
+    function handleInputChange(event) {
+        const { value } = event.target;
+        setInputValue(value);
     }
-  }
 
-  function handleDeleteIngredient(index) {
-    const updatedIngredients = [...ingredients];
-    updatedIngredients.splice(index, 1);
-    setIngredients(updatedIngredients);
-  }
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            // Remove the trailing enter character and add the ingredient to the list
+            const ingredient = inputValue.trim();
+            if (ingredient !== '') {
+                setIngredients(prevIngredients => [...prevIngredients, ingredient]);
+            }
 
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Enter ingredients" />
-      
-      <h3>Selected Ingredients:</h3>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>
-            {ingredient}
-            <button onClick={() => handleDeleteIngredient(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+            // Reset the input value
+            setInputValue('');
+        }
+    }
 
-      {/* <div>
+    function handleDeleteIngredient(index) {
+        const updatedIngredients = [...ingredients];
+        updatedIngredients.splice(index, 1);
+        setIngredients(updatedIngredients);
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                placeholder="Enter ingredients and press Enter"
+            />
+
+            <h3>Selected Ingredients:</h3>
+            <ul>
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                        {ingredient}
+                        <button onClick={() => handleDeleteIngredient(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+
+            {/* <div>
         <ul>
           {ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
           ))}
         </ul>
       </div> */}
-    </div>
-  );
+        </div>
+    );
 }

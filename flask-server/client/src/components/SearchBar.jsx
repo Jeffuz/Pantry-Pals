62028e79dec11c85c881ea7d77e88e61fd5b2741
Jeffuz <jeffzhang0049@gmail.com>
2023-06-26 23:27:58 -0700

    syntax

--- flask-server/client/src/components/SearchBar.jsx
+++ flask-server/client/src/components/SearchBar.jsx
@@ -28,7 +28,7 @@ export function SearchBarComponent() {
         <div>
             <form onSubmit={handleSearchSubmit} autoComplete="off">
                 <div class='flex space-x-4'>
-                    <input 
+                    <input
                         class="transition duration-200 delay-100 bg-gray-200 hover:bg-gray-100 border-none w-96 px-14 py-2 pr-20 rounded text-sm focus:outline-none"
                         placeholder="Enter You Food here!"
                         type="text"
@@ -40,7 +40,7 @@ export function SearchBarComponent() {
                             Search
                         </p>
                     </button>
-                    
+
                 </div>
             </form>
         </div>
--- flask-server/client/src/pages/Home.js
+++ flask-server/client/src/pages/Home.js
@@ -23,7 +23,7 @@ export default function Home() {
 
     return (
         <div className="h-screen bg-cover bg-center"
-        style={{ backgroundImage: `url(${backgroundImage})` }}>
+            style={{ backgroundImage: `url(${backgroundImage})` }}>
             {/* Log In Button */}
             <Link to="/login">
                 <button className="transition duration-200 delay-100 bg-gray-300 hover:bg-gray-500 text-gray-800  hover:text-gray-100 font-bold py-1 px-4 rounded inline-flex items-center absolute right-5 top-5">
@@ -36,15 +36,15 @@ export default function Home() {
             <div className="ml-10 absolute top-14">
                 <div className="mt-14">
                     <p className="text-8xl text-gray-50 font-serif text-left">
-                        Pantry 
-                        
+                        Pantry
+
                     </p>
                     <div className="mt-5 flex space-x-4">
                         <p className="text-8xl text-gray-50 font-serif text-left">
-                            Pals  
+                            Pals
                         </p>
                         <p className="text-2xl text-gray-50 font-serif mt-7">
-                            Fresh Ideas, <br/> Straight from your Fridge.
+                            Fresh Ideas, <br /> Straight from your Fridge.
                         </p>
                     </div>
                 </div>

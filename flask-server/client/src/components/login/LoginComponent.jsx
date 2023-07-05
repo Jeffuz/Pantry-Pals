02fd4076e244a6fae3234d0b3a38d2b8397cb6bc
Simon Huang <huisimonliu@gmail.com>
2023-07-05 11:47:56 -0700

    Updated Login component
    Can display error message in login component and updated the render html to use forms.

--- flask-server/client/src/components/login/LoginComponent.jsx
+++ flask-server/client/src/components/login/LoginComponent.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function setToken(userToken) {
-
-}
 function getToken() {
   const tokenString = sessionStorage.getItem('token');
   return tokenString;
@@ -13,8 +10,12 @@ export default function LoginComponent() {
 
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
-  
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const token = getToken();
+  const navigate = useNavigate();
+
+  // Debugging
   console.log("token", token);
   if(token) {
     console.log("logged in as", token);
@@ -22,10 +23,12 @@ export default function LoginComponent() {
   else {
     console.log("Need to log in");
   }
-  const display = event => {
-    console.log(username, password);
-  }
+  //End of Debug
 
+  function clearSessionData()
+  {
+    sessionStorage.clear();
+  }
   // Fetch data from database with username and password
   async function loginUser(credentials) {
     return fetch(`http://localhost:5000/login?`, {
@@ -50,23 +53,30 @@ export default function LoginComponent() {
     // Data Received can set state
     console.log("token=", data);
 
-    if(data.id != '') {
+    if(data.id !== '') { // Login Success
       console.log("Logged in", data.id);
       sessionStorage.setItem('token', data.id);
-
+      // Return back to home / prior tab
+      navigate(-1);
     }
+    else {// Failed Login
+      setErrorMessage("Incorrect Username or Password");
+    }
+
   }
 
   return(
     <div id="Login-Wrapper">
       <h3>Login</h3>
-
-      <input type="text" onChange={e => setUsername(e.target.value) }/>
-      <br/>
-      <input type="password" onChange={e => setPassword(e.target.value) }/>
-      <br/>
-      <button onClick={handleLogin}>Login</button>
+      <form>
+        <label for="uname">Username:</label> <br/>
+        <input id="uname" type="text" onChange={e => setUsername(e.target.value) }/> <br/>
+        <label for="pname">Password:</label> <br/>
+        <input id="pname" type="password" onChange={e => setPassword(e.target.value) }/> <br/>
+        <label id="errorMessage">{errorMessage}</label> {errorMessage !== null ? <br/> : ''}
+        <button onClick={handleLogin}>Login</button>
+      </form>
+      <button onClick={clearSessionData}>Debug Remove SessionData</button>
     </div>      
-    
   );
 }
